3a32219e382f737df117683fc0b9377260823401
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-01 01:24:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,7 +36,7 @@ def bulk_tweet_shortener(array_of_tweets)
 end #end bulk_tweet_shortener
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140
+  if tweet.size > 140
     puts word_substituter(tweet)
   else 
     puts tweet
