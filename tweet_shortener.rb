49bb8f20af06d1404381286a8d5835c5449b6ad0
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-01 00:30:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def dictionary 
+  words = {
+    "hello" => "hi", 
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "you"=> "u",
+    "at" => "@",
+    "and" => "&",
+  } # end hash words 
+  word
+end #end dictionary
+
+def word_substituter(tweet)
+  tweetArray = tweet.split
+  dictHash = dictionary
+end #end word_substituter
