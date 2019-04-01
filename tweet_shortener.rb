b7591f385705433a0e1435e57353bc3fff178a3c
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-01 01:03:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,8 +2,11 @@
 def dictionary 
   words = {
     "hello" => "hi", 
-    "to, two, too" => "2",
-    "for, four" => "4",
+    "to" => '2',
+    "two" => '2',
+     "too" => '2',
+    "for" => '4',
+    "four" => '4',
     "you"=> "u",
     "at" => "@",
     "and" => "&",
@@ -13,39 +16,12 @@ end #end dictionary
 
 def word_substituter(tweet)
   tweetArray = tweet.split
-  #dictArray = dictionary
-  dicArray = dictionary.keys 
+  keyArray = dictionary.keys
   valueArray = dictionary.values
-  totwotoo = dicArray[1]
-  new_two = totwotoo.split(", ")
-  dicArray.delete_at(1)
-  dicArray << new_two
-  new_four = dicArray[1].split(", ")
-  dicArray.delete_at(1)
-  dicArray << new_four
-  #puts dicArray
-  short_tweet = tweetArray
-  #puts "im here"
-  i = 0
-  while i < tweetArray.size
   dictionary.each do |key, value|
-  #puts "im here now
-  tweetArray.each do |x|
-      if key == x
-      #puts value
-        short_tweet.delete_at(i)
-        short_tweet.insert(i, value)
-        #puts short_tweet.join + "   im here"
-        i = i + 1 
-        #puts short_tweet
-      else 
-        i = i+1
-      #puts short_tweet
-      end #end if  
-  end #do 
-    end #do 
-    end #while 
-  short_tweet.join(" ")
-  #puts tweetArray
-  #puts dicArray
-end #end word_substituter
+  tweetArray. each do |word|
+    tweet.sub!(key, value)    
+  end #do
+  end #do
+  tweet 
+end #end word_substitute
